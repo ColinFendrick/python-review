c2ef11a8c357f4e1eedb2d48fd7bb6f22faa6681
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-15 10:37:29 -0400

    Type sort thing

---
+++ 2000/types-sort.py
@@ -0,0 +1,8 @@
+zTuple = ('Mr. Ed', 'Miss Daisy', 'Mr. T', 'Dr. Who')
+zList = (['Mr. Ed', 'Miss Daisy', 'Mr. T', 'Dr. Who'])
+zSet = ({'Mr. Ed', 'Miss Daisy', 'Mr. T', 'Dr. Who'})
+zDict = ({3: 'Mr. Ed', 2: 'Miss Daisy', 4: 'Mr. T', 1: 'Dr. Who'})
+print(sorted(zTuple))
+print(sorted(zList))
+print(sorted(zSet))
+print(sorted(zDict.items()))
