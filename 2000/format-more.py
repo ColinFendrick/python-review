929ea3bf5dc54a2593a3f83cce6fd569c73ce9dc
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-15 10:55:04 -0400

    Make a cool banner print fn

--- 2000/format-more.py
+++ 2000/format-more.py
@@ -4,18 +4,23 @@ print('Second: {1}, First: {0}'.format(22, 42))
 print('First: {foo}, Second: {bar}'.format(foo='foo', bar='bar'))
 print('Second: {bar}, First: {foo}'.format(foo='foo', bar='bar'))
 
-print('Name [{0:>12s}], Age: {1:<d}, Balance: ${2:<7.2f}'.format('Client', 62, 22.42))
+print('Name [{0:>12s}], Age: {1:<d}, Balance: ${2:<7.2f}'.format(
+    'Client', 62, 22.42))
 print('Reusing single arg: {0:6.2f} or {0:6.3f}'.format(5.1234))
 print('Ignoring multiple: {0:6.2f} or {0:6.3f}'.format(5.1234, 1.2345))
+
 
 def showChars(num, token):
     return "\t{}".format(token * (num + 4))
 
+
 def show(message):
     char = '¥'
+    # Add in our own spaces and make string uppercase
     message = '  {}  '.format(message.strip().upper())
-    stars = showChars(len(message), char)
-    print('{0}\n\t{2}{2}{1:%<s}{2}{2}\n{0}'.format(stars, message, char))
+    charLine = showChars(len(message), char)
+    print('{0}\n\t{2}{2}{1}{2}{2}\n{0}'.format(charLine, message, char))
+
 
 show('special')
-show('asdfouas0dfusaldjfl2348u23lnfsad')
+show('    asdfouas0dfusaldjfl2348u23lnfsad   ')
